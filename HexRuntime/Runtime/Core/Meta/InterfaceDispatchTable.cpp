3fc38f62993f00ad9b5828063d2feb555befbe5c
HexJacaranda <rbqlzg1937n@msn.com>
2021-08-16 14:15:25 +0800

    directly use unordered_map

--- HexRuntime/Runtime/Core/Meta/InterfaceDispatchTable.cpp
+++ HexRuntime/Runtime/Core/Meta/InterfaceDispatchTable.cpp
@@ -1,76 +1,18 @@
 #include "InterfaceDispatchTable.h"
 
-bool RTM::InterfaceDispatchTable::IsHashMode() const
-{
-	return !!(mCount & 0x80000000);
-}
-
 RT::Int32 RTM::InterfaceDispatchTable::GetCount() const
 {
-	return mCount & 0x7FFFFFFF;
+	return mMapping.size();
 }
 
 RT::Int32 RTM::InterfaceDispatchTable::GetIndex(Int32 interfaceIndex, MDToken token)
 {
 	Int64 key = (((Int64)interfaceIndex) << 32) | (Int64)token;
-	Int32 count = GetCount();
-	if (IsHashMode())
-	{
-		Int32 index, startIndex;
-		Int32 hash = GetHashCodeOf(key);
-		index = hash;
-		startIndex = hash % count;
-		do 
-		{
-			index = index % count;
-			auto&& slot = mSlots[index];
-			if (slot.GetHash() == hash && slot.Key == key)
-				return slot.Index;
-			index++;
-		} while (index != startIndex);
-	}
-	else
-	{
-		for (Int32 i = 0; i < count; ++i)
-			if (mSlots[i].Key == i)
-				return mSlots[i].Index;
-	}
-	return -1;
+	return mMapping.find(key)->second;
 }
 
 void RTM::InterfaceDispatchTable::Put(Int32 interfaceIndex, MDToken token, Int32 methodIndex)
 {
 	Int64 key = (((Int64)interfaceIndex) << 32) | (Int64)token;
-	Int32 index, startIndex;
-	Int32 hash = GetHashCodeOf(key);
-	Int32 count = GetCount();
-	index = hash;
-	startIndex = hash % count;
-	do
-	{
-		index = index % count;
-		auto&& slot = mSlots[index];
-		if (slot.GetHash() == 0)
-		{
-			slot.Key = key;
-			slot.Index = methodIndex;
-			break;
-		}
-		index++;
-	} while (index != startIndex);
-}
-
-RT::Int32 RTM::InterfaceDispatchTable::GetHashCodeOf(Int64 key)
-{
-#define XOR_SHIFT(n, i)  (n) ^ ((n) >> i) 
-	UInt64 p = 0x5555555555555555ull; 
-	UInt64 c = 17316035218449499591ull;
-	Int32 hash = 0;
-	while (hash == 0)
-	{
-		UInt64 full = c * XOR_SHIFT(p * XOR_SHIFT(key, 32), 32);
-		hash = (Int32)(full & 0x7FFFFFFF);
-	}	
-#undef XOR_SHIFT
-	return hash;
+	mMapping.insert_or_assign(key, methodIndex);
 }
--- HexRuntime/Runtime/Core/Meta/InterfaceDispatchTable.h
+++ HexRuntime/Runtime/Core/Meta/InterfaceDispatchTable.h
@@ -1,38 +1,19 @@
 #pragma once
 #include "..\..\RuntimeAlias.h"
+#include <unordered_map>
 
 namespace RTM
 {
-	struct DispatchSlot
-	{
-		//[Interface Index][MethodDef Token]
-		Int64 Key;
-		Int32 Index;
-		
-		inline Int32 const& GetHash()const
-		{
-			return *((Int32*)this - 1);
-		}
-		inline Int32& GetHash()
-		{
-			return *((Int32*)this - 1);
-		}
-	};
-
 	/// <summary>
 	/// Kind of hashtable responsible for (Interface Index, Method Token) -> (MethodTable Index) mapping.
-	///	When it's small, it behaves like a array on which we do linear search
 	/// </summary>
 	class InterfaceDispatchTable
 	{
 		friend class MetaManager;
-		DispatchSlot* mSlots;
-		//Highest bit is reserved for mode bit(linear or hash)
-		Int32 mCount;
+
+		std::unordered_map<Int64, Int32> mMapping;
 		void Put(Int32 interfaceIndex, MDToken token, Int32 index);
-		Int32 GetHashCodeOf(Int64 key);
 	public:
-		bool IsHashMode()const;
 		Int32 GetCount()const;
 		Int32 GetIndex(Int32 interfaceIndex, MDToken token);	
 	};
