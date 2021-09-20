9e9e45bbe8ddb3b73b737998a89adbc31861acaf
Hex <rbqlzg1937n@msn.com>
2021-09-20 23:18:26 +0800

    add Test to metamanager

---
+++ RuntimeTest/MetaTest.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include <filesystem>
+#include "CppUnitTest.h"
+#include "../HexRuntime/Runtime/RuntimeAlias.h"
+#include "../HexRuntime/Runtime/Core/Meta/MetaManager.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace RTM;
+
+namespace RuntimeTest
+{
+	TEST_CLASS(MetaTest)
+	{
+	public:
+		
+		TEST_METHOD(MetaManagerLoadingTest)
+		{
+			MetaData = new MetaManager();
+			auto context = MetaData->StartUp(Text("HexRT.Core"));
+			auto type = MetaData->GetTypeFromToken(context, 0);
+		}
+	};
+}
